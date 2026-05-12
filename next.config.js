9793a294b1f4c28c9157e1b8bf9e930e30431a5e
DestinMincy <destin.mincy@protonmail.com>
2026-05-12 08:39:54 -0400

    Remove unnecessary files

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ["@refinedev/antd", "antd", "@ant-design/icons", "@ant-design/cssinjs", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip"],
-};
-
-module.exports = nextConfig;
